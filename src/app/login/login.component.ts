d13a3b385e47df0e770bafd9178b81e130304073
Igor marques <igormarquespe@gmail.com>
2022-12-30 03:22:28 -0300

    Login Component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
+  
+  msgErro: string = '';
+   
   constructor() { }
 
   ngOnInit(): void {
