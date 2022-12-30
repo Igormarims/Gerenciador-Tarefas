fd4c3c40c20ea3a8158ad9cb929dcae219071707
Igor marques <igormarquespe@gmail.com>
2022-12-30 01:35:48 -0300

    Style Settings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { PainelComponent } from './painel/painel.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: PainelComponent, pathMatch: 'full'},
+  {path:'login', component: LoginComponent},
+  {path: '**', redirectTo: '/'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { PainelComponent } from './painel/painel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PainelComponent
   ],
   imports: [
     BrowserModule,
